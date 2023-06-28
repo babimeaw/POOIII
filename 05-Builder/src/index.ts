40e908e4a21aa30ddaca183035fb36705800578c
Tamilyn <128110443+babimeaw@users.noreply.github.com>
2023-06-28 18:06:57 -0300

    Update index.ts

--- 05-Builder/src/index.ts
+++ 05-Builder/src/index.ts
@@ -1,2 +1,37 @@
-const disciplina : string = "Programação Orientada a Obj III"
-console.log(disciplina)
+import SandwichBuilder from "./builders/SandwichBuilder";
+import diretor from "./directors/diretor";
+import sanduiche from "./productos/sanduiche";
+
+const builder : SandwichBuilder = new SandwichBuilder();
+const director : diretor = new diretor(builder);
+
+diretor.constructNatural();
+const natural : sanduiche= builder.sanduiche();
+imprimirDetalhes(NATURAL);
+
+director.constructVegetariano();
+const vegetariano : sanduiche = builder.getsanduiche();
+imprimirDetalhes(VEGETARIANO);
+
+director.cosntructQuaresma();
+const quaresma : sanduiche = builder.getsanduiche();
+imprimirDetalhes(QUARESMA);
+
+director.cosntructXFrango();
+const xfrango : sanduiche = builder.getsanduiche();
+imprimirDetalhes(XFRANGO);
+
+director.cosntructCarne();
+const carne : sanduiche = builder.getsanduiche();
+imprimirDetalhes(CARNE);
+
+function imprimirDetalhes(lanche : sanduiche){
+console.log("Preparando Lanche: " + lanche.tipoSanduiche);
+console.log("Pão: " + lanche.pao);
+console.log("Proteína: " + lanche.proteina);
+console.log("Salada: " + lanche.salada);
+console.log("Molhos: ");
+for (let molho of lanche.molho){
+console.log(molho.molho)
+}
+}
